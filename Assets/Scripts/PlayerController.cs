7af157f557a744673abd3ee6ae916ead8450f4e0
Jamee Kim <jameekim@nodaplife.me>
2020-02-14 15:55:09 -0500

    Add fire interval to laser shots

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float minX, maxX, minY, maxY;
+    public Transform laserSpawn;
+    public GameObject laser;
+    public float fireInterval = 0.25f;
 
     private Rigidbody2D rigidBody;
+    private float timer = 0f;
 
     void Start()
     {
@@ -23,5 +27,17 @@ public class PlayerController : MonoBehaviour
         rigidBody.position = new Vector2(
             Mathf.Clamp(currentPosition.x, minX, maxX),
             Mathf.Clamp(currentPosition.y, minY, maxY));
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Fire1") > 0f && timer > fireInterval)
+        {
+            GameObject obj = Instantiate(laser, laserSpawn.position, laserSpawn.rotation);
+            obj.transform.Rotate(0f, 0f, -90f);
+            timer = 0f;
+        }
+
+        timer += Time.deltaTime;
     }
 }
