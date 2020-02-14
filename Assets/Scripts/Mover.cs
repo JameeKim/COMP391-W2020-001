ef83f64ce09e101fa72dab2579e821f6229ec470
Jamee Kim <jameekim@nodaplife.me>
2020-02-14 13:28:53 -0500

    Add laser

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(speed, 0f);
+    }
+}
