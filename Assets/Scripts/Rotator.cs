c9981711b7d688c62b34e1da9b65702272ccc0cb
Jamee Kim <jameekim@nodaplife.me>
2020-03-06 13:34:51 -0500

    Add rotation to enemies

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float tumble = 200f;
+
+    private Rigidbody2D rigidBody;
+
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.angularVelocity = Random.value * tumble;
+    }
+}
