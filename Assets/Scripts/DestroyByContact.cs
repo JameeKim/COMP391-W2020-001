2d393ddca4c783db6ad0cdb087f3b4e558ad5d07
Jamee Kim <jameekim@nodaplife.me>
2020-03-06 13:51:57 -0500

    Make enemies be destroyed when hit by lasers

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Transform transformCache = transform;
+        Instantiate(explosion, transformCache.position, transformCache.rotation);
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
