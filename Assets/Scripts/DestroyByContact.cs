16a34148bf61620fc737668a56bf8832000e6944
Jamee Kim <jameekim@nodaplife.me>
2020-03-13 14:00:07 -0400

    Make player explode when in contact with the enemies

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,11 +3,17 @@
 public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
+    public GameObject playerExplosion;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         Transform transformCache = transform;
         Instantiate(explosion, transformCache.position, transformCache.rotation);
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
 
         Destroy(other.gameObject);
         Destroy(gameObject);
